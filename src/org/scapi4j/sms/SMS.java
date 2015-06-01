a368334ce176926c77f874530271a6eb53797c71
TZHROTH3 <thomas.roseng@gmail.com>
2015-06-01 12:25:28 +0200

    add helper methodes to SMS class

--- src/org/scapi4j/sms/SMS.java
+++ src/org/scapi4j/sms/SMS.java
@@ -1,5 +1,60 @@
 package org.scapi4j.sms;
 
-public class SMS extends SMSImpl {
+import java.util.ArrayList;
+import java.util.List;
+
+public class SMS {
+	private SMSImpl sms = new SMSImpl();
+
+	/**
+	 * 
+	 */
+	SMS() {
+		SMSOutboundMessageRequest smsOutboundMessage = new SMSOutboundMessageRequest();
+		smsOutboundMessage.setAddress(new ArrayList<String>());
+		SMSOutboundTextMessage sMSOutboundTextMessage = new SMSOutboundTextMessage();
+		smsOutboundMessage.setOutboundSMSTextMessage(sMSOutboundTextMessage);
+		sms.setOutboundSMSMessageRequest(smsOutboundMessage);
+	}
+
+	/**
+	 * 
+	 * @param receiverAddress
+	 */
+	public void addReceiver(String receiverAddress) {
+		List<String> list = sms.getOutboundSMSMessageRequest().getAddress();
+		list.add(receiverAddress);
+	}
+
+	/**
+	 * 
+	 * @param textMessage
+	 */
+	public void setTextMessage(String textMessage) {
+		if (sms != null) {
+			if (sms.getOutboundSMSMessageRequest() != null) {
+				if (sms.getOutboundSMSMessageRequest()
+						.getOutboundSMSTextMessage() != null)
+					sms.getOutboundSMSMessageRequest()
+							.getOutboundSMSTextMessage()
+							.setMessage(textMessage);
+
+			}
+		}
+
+	}
+
+	/**
+	 * 
+	 * @param senderAdress
+	 */
+	public void setSenderAddress(String senderAdress) {
+		if (sms != null) {
+			if (sms.getOutboundSMSMessageRequest() != null) {
+				sms.getOutboundSMSMessageRequest()
+						.setSenderAdress(senderAdress);
+			}
+		}
+	}
 
 }
